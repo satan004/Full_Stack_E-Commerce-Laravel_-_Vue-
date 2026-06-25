20c0459d6df44fe9f3b6e583960770b703f83300
satan004 <satan.buntit@student.passerellesnumeriques.org>
2026-06-25 21:40:08 +0700

    add

--- frontend/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { routes } from '@routes/routes.ts'
-
-const router = createRouter({
-	history: createWebHistory(import.meta.url),
-	routes,
-})
-
-export default router 
